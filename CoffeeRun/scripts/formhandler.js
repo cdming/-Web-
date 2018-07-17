345597fa4c9a0846fd8ab3eea07ece7572a12728
cdming <cdm951021@gmail.com>
2018-07-17 17:35:57 +0800

    更新handler和mian.js

--- CoffeeRun/scripts/formhandler.js
+++ CoffeeRun/scripts/formhandler.js
@@ -6,10 +6,50 @@
  (function (window) {
      'use strict';
      var App = window.App || {};
+    //  导入jQuery时。它创建了一个名为jQuery的函数，以及一个指向该函数的名为$的变量
+    // 为了一致性，应该在导入window.jQuery的时候将其分配给本地变量$
      var $ = window.jquery;
-     function FormHandler() {
-         //code 
+     function FormHandler(selector) {
+         if (!selector) {
+            //  Error 是一种内置类型 可明确表示代码中出现了意外的值或条件
+             throw new Error("No selector provided.");
+         }
+        //  带$前缀的变量是通过jQuery选择出来的元素 虽然使用jQuery时不是必须要使用这个
+         this.$formElement =$ (selector);
+         if (this.$formElement.length === 0) {
+             throw new Error ('Could Not find element with selector:'+ selector);
+         }
      }
+    //  添加提交处理程序
+    // 每当表单的submit事件在浏览器中被触发时，submit事件处理程序回调就会被执行
+    // 此时希望调用fn函数
+     FormHandler.prototype.addSubmitHandler = function (fn) {
+         console.log('Setting submit handler for form');
+        //  用到jQuery的on方法
+        // on方法接受一个事件名称，并在事件被触发时执行回调
+        // 回调函数应该接受该事件的事件对象
+        this.$formElement.on('submit',function (event) {
+            event.preventDefault();
+
+            // 提取数据
+            // 使用serializeArray 需要使用jQuery包装表单
+            // 调用$(this)会返回一个包装对象 这个包装对象可以使用serializeArray方法
+            // serializeArray 以对象数组的形式返回表单数据
+            // var data = $(this).serializeArray();
+            var data = {};
+            // 遍历数组 并复制每个元素的值
+            $(this).serializeArray().forEach(function (item) {
+                data[item.name] = item.value;
+                console.log(item.name + 'is'+item.value);
+                
+            });
+            console.log(data);
+            // 在formhandler.js的提交处理程序回调中调用fn 并把包含用户输入的数据对象传递给fn
+            fn(data);
+        });
+         
+     };
+
      App.FormHandler = FormHandler;
      window.App = App;
  })(window);
--- CoffeeRun/scripts/main.js
+++ CoffeeRun/scripts/main.js
@@ -4,14 +4,28 @@
  */
 (function (window) {
     'use strict';
-    var App = window.App || {};
+    var FORM_SELECTOR = '[data-coffee-order="form"]';
+    var App = window.App;
     var Truck = App.Truck;
     var DataStore = App.DataStore;
-
+    var FormHandler = App.FormHandler;
     var myTruck = new Truck('forfly', new DataStore());
 
     // 由于变量是在mian的函数内部声明的，而函数的外部（甚至控制台）都无法访问函数内部的变量
     // 所以为了便于交互 在main中将Truck暴露到全局命名空间中 即window
     window.myTruck = myTruck;
+    // 使用FORM_SELECTOR作为构造函数的参数调用FormHandler构造函数，
+    // 这样可以确保FormHandler实例与选择器选出的DOM元素绑定在一起
+    var formHandler = new FormHandler(FORM_SELECTOR);
+    // 下一行是修改前的代码
+    // formHandler.addSubmitHandler();
+    // createOrder在事件处理回调中被调用时，它的所有者会发生变化
+    // 此时createOrder内部的this将不在是Trunk实例
+    // 应该把myTruck.createOrder的所有者绑定为myTruck，然后再把这个函数传递给FormHandler.addSubmitHandler
+    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+    console.log(formHandler);
+    
+
+
 
 })(window);
