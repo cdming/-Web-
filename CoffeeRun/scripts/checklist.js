5cec20b206def76346fb09173211916b8ac0b033
cdming <cdm951021@gmail.com>
2018-07-18 18:29:41 +0800

    添加Row

--- CoffeeRun/scripts/checklist.js
+++ CoffeeRun/scripts/checklist.js
@@ -1,6 +1,8 @@
 /**
  * CheckList 需要3个方法来完成工作 
- * 
+ * 1.负责创建一个清单项 包括了复选框和描述文本
+ * 2.会从table中移除一项
+ * 3.为单击事件添加一个监听器 从而让代码知道何时需要移除一项
  */
 
 (function (window) {
@@ -17,6 +19,15 @@
             throw new Erroe("Could not findelement with selector:"+ selector +);
         }
     }
+
+    // 构造Row 构造函数
+    function Row(coffeeOrder) {
+        var $div = $('<div></div>',{
+            'data-coffee-order': 'checkbox','class':'checkbox'
+        });
+    }
+
+
     App.CheckList = CheckList;
     window.App = App;
 
